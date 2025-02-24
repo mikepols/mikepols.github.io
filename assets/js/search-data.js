1d04f78686ec8701cacdc245d15e03b1ed5869de
mikepols <35675479+mikepols@users.noreply.github.com>
2025-02-24 09:53:14 +0000

    Deploying to gh-pages from @ mikepols/mikepols.github.io@9a2dc5a46f025142e9a860df6ec6898d174f35d9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -58,6 +58,9 @@ ninja.data = [{
           section: "News",},{id: "news-a-new-preprint-covering-chiral-phonons-in-halide-perovskites-was-published-on-arxiv",
           title: 'A new preprint covering chiral phonons in halide perovskites was published on arXiv!...',
           description: "",
+          section: "News",},{id: "news-today-i-successfully-defended-my-phd-cum-laude-a-news-article-about-the-research-done-during-these-four-years-from-cursor-can-be-found-here",
+          title: 'Today I successfully defended my PhD cum laude! (🎓) A news article about...',
+          description: "",
           section: "News",},{
       id: 'light-theme',
       title: 'Change theme to light',
